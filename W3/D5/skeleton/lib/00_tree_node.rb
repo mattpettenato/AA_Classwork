3d179db0a241873c72cc2ed60950fee6ad0037ed
mattpettenato <mattpettenato@gmail.com>
2021-09-17 16:05:18 -0700

    Worked with Alex Lolas

--- W3/D5/skeleton/lib/00_tree_node.rb
+++ W3/D5/skeleton/lib/00_tree_node.rb
@@ -1,49 +1,16 @@
 class PolyTreeNode
-
+  attr_reader  :children, :parent
   attr_accessor :value
-  attr_reader :parent, :children
-
-  def initialize(value = nil)
+  def initialize(value)
     @value = value
     @parent = nil
     @children = []
   end
 
-  def parent=(parent)
 
+  def parent=(value)
+    @parent = value
   end
 
-  def add_child(child)
-    child.parent = self
-  end
-
-  def remove_child(child)
-    if child && !self.children_include?(child)
-      raise 'node is not a child'
-    end
-
-    child.parent = nil
-  end
-
-  private
-  a
-
+ 
 end
-
-def bfs(val)
-  # create a queue with node inside
-  queue MyQueue.new
-  queue.enqueue(self)
-
-  until queue.empty?
-    cyr_node = queue.dequeue # take front of queue out of line
-    return cur_node if cur_node.value == val # return node if its value is what we are looking for
-    cur_node.children.each do |node| # adds children to the queue if we did not match
-      queue.enqueue(node)
-    end
-  end
-
-  nil
-end
-
-
