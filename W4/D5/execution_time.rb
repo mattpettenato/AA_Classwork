211bd6d4959041b50be281a6adfdbea38a51ec2f
mattpettenato <mattpettenato@gmail.com>
2021-09-24 14:47:30 -0700

    Finished Execution Time

--- W4/D5/execution_time.rb
+++ W4/D5/execution_time.rb
@@ -18,22 +18,51 @@ def my_min(arr)
   min
 end
 
-# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min(list)  # =>  -5
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)  # =>  -5
 
 def largest_contiguous_subsum(arr)
-  max = 0
-  max2 = 0
-  i = 0
-  while i < arr.length
-    if arr[i] <= max
-      max2 = max
-      max = arr[i]
+  subs = []
+  arr.each.with_index do |i|
+    (i...arr.length).each.with_index do |j|
+      subs << arr[i..j]
     end
-    i += 1
   end
-  max2 + max
+  max = 0
+  subs.each do |ele|
+    if ele.sum >= max
+      max = ele.sum
+    end
+  end
+  max
 end
 
+
+# O(2n^3)
+
+def largest_contiguous_subsum(arr)
+  max_sum = arr.first #
+  current_sum = 0  # 0
+  arr.each do |el| # 
+    current_sum += el
+    #max_sum = current_sum if max_sum.nil?
+    if current_sum < el
+      current_sum = el
+    end
+    if current_sum > max_sum
+      max_sum = current_sum
+    end
+  end
+  max_sum
+end
+
+# O(n)
+
 list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+list = [1, 2]
 p largest_contiguous_subsum(list)
