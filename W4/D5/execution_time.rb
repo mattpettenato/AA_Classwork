06b8d033864ce58ede4680009da48f51f874b0a2
tharpercp <tharpercp@gmail.com>
2021-09-24 12:25:16 -0700

    cotig sub_sum

--- W4/D5/execution_time.rb
+++ W4/D5/execution_time.rb
@@ -1,8 +1,39 @@
+# def my_min(arr)
+#   arr.each do |el| #n
+#   arr.all?{|x| el <= x } #n
+#   end 2n => n
+# end
+
+
+
 def my_min(arr)
-  arr.each do |el|
-    return el if arr.all?{|x| el <= x }
+  min = 0
+  i = 0
+  while i < arr.length
+    if arr[i] <= min
+      min = arr[i]
+    end
+    i += 1
   end
+  min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+def largest_contiguous_subsum(arr)
+  max = 0
+  max2 = 0
+  i = 0
+  while i < arr.length
+    if arr[i] <= max
+      max2 = max
+      max = arr[i]
+    end
+    i += 1
+  end
+  max2 + max
+end
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list)
