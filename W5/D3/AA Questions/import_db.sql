9b1ef5c9132f0745b628dfeb3babb229ac32eb13
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-09-29 11:35:00 -0700

    Work on project(s)

--- W5/D3/AA Questions/import_db.sql
+++ W5/D3/AA Questions/import_db.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS question_likes
-DROP TABLE IF EXISTS replies
-DROP TABLE IF EXISTS question_follows
-DROP TABLE IF EXISTS questions
-DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 PRAGMA foreign_keys = ON;
 
@@ -29,8 +29,29 @@ CREATE TABLE questions (
 INSERT INTO
   questions (title, body, author_id)
 SELECT
-  ('Lebron Question', 'Lebron Lebron Lebron', 1)
+  'Lebron Question', 'Lebron Lebron Lebron', users.id
 FROM
+  users
+WHERE
+  users.fname = 'Lebron' AND users.lname = 'James';
+
+INSERT INTO
+  questions (title, body, author_id)
+SELECT
+  'Mark Question', 'Mark Mark Mark', users.id
+FROM
+  users
+WHERE
+  users.fname = 'Mark' AND users.lname = 'Twain';
+
+INSERT INTO
+  questions (title, body, author_id)
+SELECT
+  'Andy Question', 'Andy Andy Andy', users.id
+FROM
+  users
+WHERE
+  users.fname = 'Andy' AND users.lname = 'Huang';
 
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
@@ -41,8 +62,18 @@ CREATE TABLE question_follows (
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES
+  ((SELECT id FROM users WHERE users.fname = 'Lebron' AND users.lname = 'James'),
+    (SELECT id FROM questions WHERE title = 'Lebron Question')),
+  ((SELECT id FROM users WHERE users.fname = 'Mark' AND users.lname = 'Twain'),
+    (SELECT id FROM questions WHERE title = 'Mark Question')),
+  ((SELECT id FROM users WHERE users.fname = 'Andy' AND users.lname = 'Huang'),
+    (SELECT id FROM questions WHERE title = 'Andy Question'));
+
 CREATE TABLE replies (
-  if INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
   parent_reply_id INTEGER,
   author_id INTEGER NOT NULL,
@@ -53,6 +84,19 @@ CREATE TABLE replies (
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
+INSERT INTO
+  replies (question_id, parent_reply_id, author_id, body)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'Lebron Question'),
+    NULL,
+    (SELECT id FROM users WHERE users.fname = 'Lebron' AND users.lname = 'James'),
+    'I am replying to your message'),
+  ((SELECT id FROM questions WHERE title = 'Mark Question'),
+    (SELECT id FROM replies WHERE body = 'I am replying to your message'),
+    (SELECT id FROM users WHERE users.fname = 'Mark' AND users.lname = 'Twain'),
+    'I am replying to your REPLY');
+
+
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
@@ -61,3 +105,9 @@ CREATE TABLE question_likes (
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO
+  question_likes (user_id, question_id)
+VALUES
+  ((SELECT id FROM users WHERE users.fname = 'Andy' AND users.lname = 'Huang'),
+    (SELECT id FROM questions WHERE title = 'Lebron Question'));
