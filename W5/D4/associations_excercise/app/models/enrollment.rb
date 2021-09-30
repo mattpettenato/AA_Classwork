cd92c73f59695988dca51142cd69c9f89c6bd8aa
mattpettenato <mattpettenato@gmail.com>
2021-09-30 14:45:10 -0700

    Set up Associations Excercise

---
+++ W5/D4/associations_excercise/app/models/enrollment.rb
@@ -0,0 +1,9 @@
+# create_table "enrollments", force: :cascade do |t|
+#     t.integer "course_id"
+#     t.integer "student_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+class Enrollment < ApplicationRecord
+end
