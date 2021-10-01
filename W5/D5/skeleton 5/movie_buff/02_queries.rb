ca25c98c2c636ea1a2846c58a50f9ebe32e1c5f9
mattpettenato <mattpettenato@gmail.com>
2021-10-01 14:00:44 -0700

    Worked on 02 que

--- W5/D5/skeleton 5/movie_buff/02_queries.rb
+++ W5/D5/skeleton 5/movie_buff/02_queries.rb
@@ -2,18 +2,26 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989, score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .having('MAX(score) < 8')
+    .group(:yr)
+    .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order('castings.ord')
 end
 
 def vanity_projects
@@ -22,7 +30,11 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+    .select('movies.id', 'movies.title', 'actors.name')
+    .joins(:actors)
+    .where('director_id = actors.id')
+    .where('castings.ord = 1')
 end
 
 def most_supportive
