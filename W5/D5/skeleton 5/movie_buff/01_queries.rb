a3c518c8c2619b101c78b651dbf6389a730a438d
mattpettenato <mattpettenato@gmail.com>
2021-10-01 12:02:08 -0700

    Worked on 01 Queries

--- W5/D5/skeleton 5/movie_buff/01_queries.rb
+++ W5/D5/skeleton 5/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,6 +20,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where.not(castings: {ord: 1})
+    .where(actors: {name: 'Harrison Ford'})
 
 end
 
