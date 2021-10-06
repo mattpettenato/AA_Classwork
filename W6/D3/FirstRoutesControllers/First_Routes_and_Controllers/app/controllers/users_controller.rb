d988399f33d7925daa015b6ef419b7b0c82503ae
Mark <mrkdawes8@gmail.com>
2021-10-06 18:32:59 -0400

    add create show

--- W6/D3/FirstRoutesControllers/First_Routes_and_Controllers/app/controllers/users_controller.rb
+++ W6/D3/FirstRoutesControllers/First_Routes_and_Controllers/app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
-  
+  def index
+    render plain: "I'm in the index action!"
+  end
+
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params
+  end
+
 end
