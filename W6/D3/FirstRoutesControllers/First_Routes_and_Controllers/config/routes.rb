4aab7e92023c32f48405c8b6c522b27ca61d05f9
mattpettenato <mattpettenato@gmail.com>
2021-10-06 14:52:16 -0700

    First Routes and Controllers

---
+++ W6/D3/FirstRoutesControllers/First_Routes_and_Controllers/config/routes.rb
@@ -0,0 +1,25 @@
+                #     users GET    /users(.:format)                                                                         users#index
+                #           POST   /users(.:format)                                                                         users#create
+                #  new_user GET    /users/new(.:format)                                                                     users#new
+                # edit_user GET    /users/:id/edit(.:format)                                                                users#edit
+                #      user GET    /users/:id(.:format)                                                                     users#show
+                #           PATCH  /users/:id(.:format)                                                                     users#update
+                #           PUT    /users/:id(.:format)                                                                     users#update
+                #           DELETE /users/:id(.:format)                                                                     users#destroy
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+  
+
+
+end
