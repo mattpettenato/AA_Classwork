1fdbafc39a511ed62b77895991acbc79bd03fc2d
mattpettenato <mattpettenato@gmail.com>
2021-10-15 15:02:36 -0700

    Created SessionsController

--- W7/D5/RedditClone/RedditClone/app/controllers/sessions_controller.rb
+++ W7/D5/RedditClone/RedditClone/app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+
+  before_action :require_logged_in, only: [:destroy]
+  before_action :require_logged_out, except: [:destroy]
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user 
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to users_url
+  end
 end
