aebfcd1a43e5ccb383df47a29198ca2fe0f9fc5a
Abraham Fong <abisfong@ucsc.edu>
2021-10-15 14:43:35 -0700

    Complete user controller actions

--- W7/D5/RedditClone/RedditClone/app/controllers/users_controller.rb
+++ W7/D5/RedditClone/RedditClone/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # before_action :require_logged_in, only: []
 
   def new
     # @user = User.new
@@ -8,33 +9,44 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      login(@user)
+      redirect_to user_url(@user)
     else
-      
+      flash[:errors] = @user.errors.full_messages
+      render :new
     end
   end
 
   def show
-
+    @user = User.find(params[:id])
+    render :show
   end
 
   def index
-
+    @users = Users.all
+    render :index
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if !@user.update(user_params)
+      flash[:errors] = @user.errors.full_messages
+    end
+    redirect_to user_url(@user)
   end
 
   def edit
-
+    @user = User.find(params[:id])
+    render :edit
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
   end
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
