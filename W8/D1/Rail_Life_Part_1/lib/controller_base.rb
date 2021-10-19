36b852fb5aafd7d29964eaacd1fc3b2acb4a952e
mattpettenato <mattpettenato@gmail.com>
2021-10-18 17:31:41 -0700

    Finished p02 controllerbase

--- W8/D1/Rail_Life_Part_1/lib/controller_base.rb
+++ W8/D1/Rail_Life_Part_1/lib/controller_base.rb
@@ -8,20 +8,31 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    #@already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.location = (url)
+    @res.status = 302
+    raise_error if !@already_built_response.nil?
+    @already_built_response ||= true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    res['Content-Type'] = 'text/html'
+    raise_error if !@already_built_response.nil?
+    @already_built_response ||= res.write(content)
   end
 
   # use ERB and binding to evaluate templates
