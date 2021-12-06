7f6e2e6bc9d05b33d7d897e5d10998feb2cc84d3
mattpettenato <mattpettenato@gmail.com>
2021-12-05 19:44:20 -0800

    set up repo final

--- MERN_Practice/MERN_Twitter/app.js
+++ MERN_Practice/MERN_Twitter/app.js
@@ -1,2 +1,17 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => res.send("Hello World but different!"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const db = require('../MERN_Twitter/config/key').mongoURI;
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
---
+++ MERN_Practice/MERN_Twitter/config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://demo:C1ZKMNExMeVjeZtU@cluster0.3xelk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+            //Make sure this is your own unique string
+}
